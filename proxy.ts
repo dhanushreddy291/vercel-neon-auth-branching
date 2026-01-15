4074f5a14e167adca404705d4e211cee9afd78ef
Dhanush Reddy <29dhanushreddy@gmail.com>
2026-01-15 17:42:35 +0530

    add neon auth to nextjs

---
+++ proxy.ts
@@ -0,0 +1,5 @@
+import { neonAuthMiddleware } from "@neondatabase/auth/next/server";
+
+export default neonAuthMiddleware({ loginUrl: "/auth/sign-in" });
+
+export const config = { matcher: ["/"] };
